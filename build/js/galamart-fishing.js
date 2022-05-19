6138a21095746f58b05261ffe52ad2601634859e
Maxim Karachyov <karachyov.m@gmail.com>
2022-05-19 10:36:32 +0300

    js

--- build/js/galamart-fishing.js
+++ build/js/galamart-fishing.js
@@ -1,22 +1,50 @@
-const start = document.getElementById('start');
-
-start.addEventListener('click', function (e){
-    const target = e.target;
-    let one = document.querySelector('.fishing-form__tab');
-    target.classList.add('d-none');
-    one.classList.add('d-block');
+const start = document.getElementById('start')
+var currentTab = 0;
+start.addEventListener('click', function (){
+    start.classList.add('d-none')
+    showTab(currentTab);
 })
 
-$(function (){
-    $('.form-menu__item').on('click', function (){
-        $(this).parents('.fishing-form__tab').addClass('d-none');
-        $(this).parents('.fishing-form__tab').next().addClass('d-block')
+function showTab(n) {
+    var x = document.getElementsByClassName("fishing-form__tab");
+    x[n].style.display = "block";
+
+    /*if (n == 0) {
+        document.getElementById("prevBtn").style.display = "none";
+    } else {
+        document.getElementById("prevBtn").style.display = "block";
+    }
+    if (n == (x.length - 1)) {
+        document.getElementById("nextBtn").innerHTML = "Submit";
+    } else {
+        document.getElementById("nextBtn").innerHTML = "Next";
+    }*/
+
+    fixStepIndicator(n)
+}
+function nextPrev(n) {
+    var x = document.getElementsByClassName("fishing-form__tab");
+    if (n == 0){
+        x[currentTab].style.display = "none";
+        currentTab = 0;
+        showTab(currentTab);
+    }
+    x[currentTab].style.display = "none";
+    currentTab = currentTab + n;
+    if (currentTab >= x.length) {
+        document.getElementById("FishingForm").submit();
         return false;
-    })
+    }
+    showTab(currentTab);
+    console.log(currentTab)
+}
 
-    $('.js-start-menu').on('click', function (){
-        let menu = $('.fishing-form__tab').data('step');
-        $('.fishing-form__tab').not().addClass('d-none');
 
-    })
-})
+
+function fixStepIndicator(n) {
+    var i, x = document.getElementsByClassName("nav-step__item");
+    for (i = 1; i < x.length; i++) {
+        x[i].className = x[i].className.replace(" nav-step__item--active    ", "");
+    }
+    x[n].className += " nav-step__item--active";
+}
